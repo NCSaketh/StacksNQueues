f84c28d1bd77ccf571dd35d13864a94a177301a0
NCSaketh <nc.saketh1@gmail.com>
2020-10-22 09:54:58 +0530

    UC2 : Peak and pop operations

--- src/main/java/com/cg/stacksnqueues/StacksNQueuesMain.java
+++ src/main/java/com/cg/stacksnqueues/StacksNQueuesMain.java
@@ -9,5 +9,22 @@ public class StacksNQueuesMain {
         s.push(30);
         s.push(56);
         s.printStack();
+        System.out.println("");
+
+        //Peak
+        System.out.println("PEAK");
+        s.peak();
+        System.out.println("");
+
+        //Pop
+        System.out.println("POP");
+        s.pop();
+        s.pop();
+        s.pop();
+        System.out.println("Every element has been popped");
+        s.printStack();
+
+        System.out.println("");
+
     }
 }
