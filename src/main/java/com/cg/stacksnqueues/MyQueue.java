0b522c19c9805415b348257a11aca974e79f67f1
NCSaketh <nc.saketh1@gmail.com>
2020-10-22 10:03:40 +0530

    UC3 : Creating queue and enqueue operation

--- src/main/java/com/cg/stacksnqueues/MyQueue.java
+++ src/main/java/com/cg/stacksnqueues/MyQueue.java
@@ -1,4 +1,15 @@
 package com.cg.stacksnqueues;
 
 public class MyQueue {
+    MyLinkedList ll=new MyLinkedList();
+
+    public void enqueue(int data)
+    {
+        Node node=new Node(data);
+        ll.append(data);
+    }
+    public void printQueue()
+    {
+        ll.print();
+    }
 }
